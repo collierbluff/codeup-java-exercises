1c277974f05f173962df847815d306e67a7f001a
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-07-23 16:50:06 -0500

    worked on java syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,35 @@ public class HelloWorld {
     public static void main(String[] args){
 
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 96;
+
+        System.out.println(myFavoriteNumber);
+
+        char myString = 'T';
+
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+
+        System.out.println(myNumber);
+
+        int m = 5;
+        System.out.println(++m);
+        System.out.println(m);
+
+        int x = 4;
+        x += 5;
+
+        int a = 3;
+        int b = 4;
+        b *= a;
+
+        int t = 10;
+        int g = 2;
+        t /= g;
+        g -= t;
+
+
     }
 }
