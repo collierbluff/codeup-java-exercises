cc22e891f1845ce39a3727059f8dab1c3d92702b
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-07-25 17:00:46 -0500

    in progress

---
+++ src/MethodsExercises.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+
+     public static Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args){
+
+        System.out.println("Please enter a number between 1 and 10");
+
+        int input = getInteger(1, 10);
+
+
+//        Part 1:
+//        int a = 5;
+//        int b = 2;
+//
+//        System.out.println("Add: " + add(a,b));
+//        System.out.println("Subtract: " + subtract(a,b));
+//        System.out.println("Multiply: " + multiply(a,b));
+//        System.out.println("Divide: " + divide(a,b));
+//        System.out.println("Modulus: " + modulus(a,b));
+
+
+
+    }
+
+    public static int getInteger(byte min, byte max){
+
+        int num = sc.nextByte();
+
+        if (num < min)
+
+    }
+
+//    public static int add(int a, int b){
+//
+//        return a + b;
+//    }
+//
+//    public static int subtract(int a, int b){
+//
+//        return a - b;
+//    }
+//
+//    public static int multiply(int a, int b){
+//
+//        return a * b;
+//    }
+//
+//    public static double divide(double a, double b){
+//
+//        return (a / b);
+//    }
+//
+//    public static int modulus(int a, int b){
+//
+//        return (a % b);
+//    }
+
+
+
+
+
+}
