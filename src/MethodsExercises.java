d5352b38d85619570b553f12be6642cab5f3a795
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-07-26 09:48:19 -0500

    first 4 method exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -7,9 +7,6 @@ public class MethodsExercises {
 
     public static void main(String[] args){
 
-        System.out.println("Please enter a number between 1 and 10");
-
-        int input = getInteger(1, 10);
 
 
 //        Part 1:
@@ -22,44 +19,128 @@ public class MethodsExercises {
 //        System.out.println("Divide: " + divide(a,b));
 //        System.out.println("Modulus: " + modulus(a,b));
 
+//        Part 2:
+//        int input = getInteger(1, 10);
+
+//        Part 3:
+//        factorial();
+
+//        Part 4:
+//        roll();
 
 
     }
 
-    public static int getInteger(byte min, byte max){
 
-        int num = sc.nextByte();
+//    Part 1:
 
-        if (num < min)
+    public static int add(int a, int b){
+
+        return a + b;
+    }
+
+    public static int subtract(int a, int b){
+
+        return a - b;
+    }
+
+    public static int multiply(int a, int b){
+
+        return a * b;
+    }
+
+    public static double divide(double a, double b){
+
+        return (a / b);
+    }
+
+    public static int modulus(int a, int b){
+
+        return (a % b);
+    }
+
+//    Part 2:
+
+    public static int getInteger(int min, int max){
+
+        System.out.println("Please enter a number between 1 and 10");
+
+        int num = sc.nextInt();
+        sc.nextLine();
+
+        if (num < min){
+
+            getInteger(min, max);
+        }else if (num > max){
+
+            getInteger(min, max);
+        }
+
+        return num;
 
     }
 
-//    public static int add(int a, int b){
-//
-//        return a + b;
-//    }
-//
-//    public static int subtract(int a, int b){
-//
-//        return a - b;
-//    }
-//
-//    public static int multiply(int a, int b){
-//
-//        return a * b;
-//    }
-//
-//    public static double divide(double a, double b){
-//
-//        return (a / b);
-//    }
-//
-//    public static int modulus(int a, int b){
-//
-//        return (a % b);
-//    }
+//    Part 3:
+
+    public static void factorial(){
+
+        boolean looping = true;
+
+        while(looping) {
+
+            int input = getInteger(1, 10);
+
+            long total = 1;
+
+            for(int i = 1; i <= input; i++){
+
+                total = total * i;
+            }
+
+            System.out.println(total);
 
 
+            System.out.println("Want to continue? (y/n)");
+
+            char yN = sc.nextLine().charAt(0);
+
+            if(yN == 'n'){
+                looping = false;
+            }
+
+        }
+
+
+    }
+
+//    Part 4:
+
+    public static void roll(){
+
+        System.out.println("Please choose how many sides are on your die:");
+
+        int diceSides = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Wanna roll the dice? (y/n)");
+
+        char yN = sc.next().charAt(0);
+        sc.nextLine();
+        boolean rolling = (yN == 'y');
+
+        while(rolling) {
+
+            System.out.println("You rolled a " + (int)((Math.random() * diceSides) + 1) + " and a " + (int)((Math.random() * diceSides) + 1));
+
+            System.out.println("Roll again? (y/n)");
+
+            yN = sc.next().charAt(0);
+            sc.nextLine();
+
+            rolling = (yN == 'y');
+        }
+
+    }
 
 
 
